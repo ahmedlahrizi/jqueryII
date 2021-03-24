77472123fec2c997ef5eaecd7337c3eee892483b
Ahmed Lahrizi <ahmed.lahrizi05@gmail.com>
2021-03-24 14:55:42 +0100

    oof

--- CODE-G05-L64/script.js
+++ CODE-G05-L64/script.js
@@ -1,16 +1,5 @@
 function on_ready() {
-    // document.querySelectorAll("h1").style.visibility = "hidden";
-    document.querySelectorAll("h1").style.display = "none";
     // $("h1").hide();
-    console.log(document.readyState);
+    $("p:nth-child(2)").hide();
 }
-
-window.addEventListener("load", on_ready);
-// $(document).ready(on_ready);
-
-// J'essaye de faire le plus de Vanilla JS possible !!
-// if (document.readyState === "ready" || document.readyState === "complete") {
-//     console.log(document.querySelectorAll("h1").style.visibility);
-// } else {
-//     console.log(document.readyState);
-// }
+$(on_ready);
