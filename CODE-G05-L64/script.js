9602011a0935f31467bb8c07d7929cf324f0783a
Ahmed Lahrizi <ahmed.lahrizi05@gmail.com>
2021-03-24 14:14:24 +0100

    changed js thing

--- CODE-G05-L64/script.js
+++ CODE-G05-L64/script.js
@@ -1,6 +1,6 @@
 function on_ready() {
     // document.querySelectorAll("h1").style.visibility = "hidden";
-    document.querySelectorAll("h1").classList.remove("is-visible");
+    document.querySelectorAll("h1").style.display = "none";
     // $("h1").hide();
     console.log(document.readyState);
 }
@@ -8,7 +8,7 @@ function on_ready() {
 window.addEventListener("load", on_ready);
 // $(document).ready(on_ready);
 
-// J'essaye de faire le plus de Vannila JS possible !!
+// J'essaye de faire le plus de Vanilla JS possible !!
 // if (document.readyState === "ready" || document.readyState === "complete") {
 //     console.log(document.querySelectorAll("h1").style.visibility);
 // } else {
