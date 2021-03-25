91482a4b99adfb970c93ecf35bdeb83fdb90f413
Ahmed Lahrizi <ahmed.lahrizi05@gmail.com>
2021-03-25 13:34:55 +0100

    hey

--- CODE-G05-L64/script.js
+++ CODE-G05-L64/script.js
@@ -1,5 +1,20 @@
-function on_ready() {
-    // $("h1").hide();
-    $("p:nth-child(2)").hide();
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
-$(on_ready);
+// The only way to get a random integer in Js 😭
+function onEnter() {
+    // No need to use Var because it's will be a global variable
+    Current_hidden_object = randomInt(2, 6);
+    console.log("Hidden paragraph number", Current_hidden_object);
+
+    $(`p:nth-child(${Current_hidden_object})`).hide();
+}
+function onLeave() {
+    $(`p:nth-child(${Current_hidden_object})`).show();
+    console.log("Showed paragraph number", Current_hidden_object);
+}
+function onReady() {
+    $("p:nth-child(1)").mouseenter(onEnter);
+    $("p:nth-child(1)").mouseleave(onLeave);
+}
+$(document).ready(onReady);
